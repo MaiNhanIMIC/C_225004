20951989be2275fa9c3c9fbfccd958db1d29b7f2
MaiNhanIMIC <mainhan.imic@gmail.com>
2025-03-06 19:48:51 +0700

    tìm số lớn nhất của a và b

--- C_225004/main.c
+++ C_225004/main.c
@@ -1,6 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
 
 void main()
 {
-	printf("xin chao \n");
+	int x = 0;
+	float y = 0;
+	printf("nhap gia tri cho x: ");
+	scanf("%d", &x);
+
+	printf("nhap gia tri cho y: ");
+	scanf("%f", &y);
+
+	printf("Hello world, gia tri x: %d, y: %f\n", x, y);
 }
