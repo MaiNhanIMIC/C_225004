0d365fd9085ad72207a70b2ae15863939ddf9d5a
MaiNhanIMIC <mainhan.imic@gmail.com>
2025-03-11 20:49:23 +0700

    mô phỏng việc trả tiền của máy ATM

--- C_225004/main.c
+++ C_225004/main.c
@@ -1,14 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <conio.h>
 
 void main()
 {
-	for (int i = 1; i <= 10; i++)
+	int so_tien_can_rut = 10000;
+	int so_to_50 = 0;
+	int so_to_100 = 0;
+	int so_to_200 = 0;
+	int so_to_500 = 0;
+	int tong_tien_atm_tra = 0;
+	while (tong_tien_atm_tra < so_tien_can_rut)
 	{
-		for (int j = 2; j <= 9; j++)
-		{
-			printf("%dx%d=%d\t", j, i, i * j);
-		}
-		printf("\n");
+		if (so_tien_can_rut - tong_tien_atm_tra >= 50)
+			so_to_50++;
+		tong_tien_atm_tra = so_to_50 * 50 + so_to_100 * 100 + so_to_200 * 200 + so_to_500 * 500;
+
+		if (so_tien_can_rut - tong_tien_atm_tra >= 100)
+			so_to_100++;
+		tong_tien_atm_tra = so_to_50 * 50 + so_to_100 * 100 + so_to_200 * 200 + so_to_500 * 500;
+
+		if (so_tien_can_rut - tong_tien_atm_tra >= 200)
+			so_to_200++;
+		tong_tien_atm_tra = so_to_50 * 50 + so_to_100 * 100 + so_to_200 * 200 + so_to_500 * 500;
+
+		if (so_tien_can_rut - tong_tien_atm_tra >= 500)
+			so_to_500++;
+		tong_tien_atm_tra = so_to_50 * 50 + so_to_100 * 100 + so_to_200 * 200 + so_to_500 * 500;
 	}
+	
 }
