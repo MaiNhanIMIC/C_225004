477e989236d9cf5b2641a83a5a1302130b0322fa
MaiNhanIMIC <mainhan.imic@gmail.com>
2025-03-11 20:02:58 +0700

    bảng cửu chương

--- C_225004/main.c
+++ C_225004/main.c
@@ -1,28 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-
 void main()
 {
-	float diem = 0;
-
-	printf("nhap diem trung binh: ");
-	scanf("%f", &diem);
-
-	if (diem >= 8.0)
+	for (int i = 1; i <= 10; i++)
 	{
-		printf("GIOI \n");
-	}
-	else if (diem >= 6.5)
-	{
-		printf("KHA \n");
-	}
-	else if (diem >= 5.0)
-	{
-		printf("TRUNG BINH \n");
-	}
-	else
-	{
-		printf("YEU \n");
+		for (int j = 2; j <= 9; j++)
+		{
+			printf("%dx%d=%d\t", j, i, i * j);
+		}
+		printf("\n");
 	}
 }
