0563f8478767270b042968b8e67d7fb77afaefa5
MaiNhanIMIC <mainhan.imic@gmail.com>
2025-03-13 20:29:37 +0700

    Tìm vị trí của số lớn nhất

--- C_225004/main.c
+++ C_225004/main.c
@@ -5,34 +5,25 @@
 
 void main()
 {
-	int n = 22345;
-	int don_vi = 0;
-	// làm sao đê biết con số n tới hàng mấy? ngàn
-	while (n / (int)pow(10, don_vi))
+	//KHAI BÁO 10 đối tượng, mỗi đối tượng có kiểu dữ liệu là int
+	int arr[] = { 1,2,2,4,4,3,1,3,2,4,1,3 };
+
+	// tìm số lớn nhất trong mãng
+	int so_lon_nhat = 0;
+	int so_luong_doi_tuong = sizeof(arr)/sizeof(arr[0]);
+	for (int i = 0; i < so_luong_doi_tuong; i++)
 	{
-		don_vi++;
-	}
-	printf("don vi: %d\n", don_vi);
-	for (int i = don_vi - 1; i >= 0; i--)
-	{
-		int tam = n / (int)pow(10, i);
-		printf("%d", tam); 
-		switch (i)
-		{
-		case 4:
-			printf(" muoi ");
-			break;
-		case 3:
-			printf(" ngan ");
-			break;
-		case 2:
-			printf(" tram ");
-			break;
-		case 1: 
-			printf(" muoi ");
-			break;
+		if (so_lon_nhat < arr[i]) {
+			so_lon_nhat = arr[i];
 		}
-		n = n - tam * pow(10, i);
 	}
-	
+
+	// in ra casc vị trí của các số bằng với số lớn nhất
+	for (int i = 0; i < so_luong_doi_tuong; i++)
+	{
+		if(arr[i] == so_lon_nhat)
+			printf("%d ", i);
+	}
+
 }
+
