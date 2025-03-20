4fce3f5fecff2b3f547fce0e66f20b9aa2b3083c
MaiNhanIMIC <mainhan.imic@gmail.com>
2025-03-20 19:19:11 +0700

    ham

--- C_225004/main.c
+++ C_225004/main.c
@@ -1,29 +1,63 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <conio.h>
-#include <math.h>
+
+
+
+// Chức năng: cộng 2 số nguyên
+// input: hai số nguyên
+//	int x: số nguyên thứ nhất
+//	int y: số nguyên thứ hai
+// output int: kêt quả
+int Cong_So_Nguyen(int x, int y)
+{
+	// output về một đối tượng int
+	int ketqua = 0;
+	ketqua = x + y;
+	return ketqua;
+}
+
+// Chức năng: in hello ra màn hình console
+// input: không có
+// output: không có
+void In_Hello()
+{
+	printf("hello\n");
+}
+
+// Chức năng: dùng để tính tổng các giá trị trong mảng char
+// input: mảng char
+//	char* dia_chi: địa chỉ bất đầu của mảng
+//	int so_luong : số lượng phần tử trong mảng
+// output: int kết quả của phép tính
+int Tong_Mang(char* dia_chi, int so_luong)
+{
+	int ketqua = 0;
+	for (int i = 0; i < so_luong; i++)
+	{
+		ketqua += *(dia_chi + i);
+		// ketqua += dia_chi[i];
+	}
+	return ketqua;
+}
+
+// Chức nâng: dùng để điếm số ký tự trong chuổi
+// input: chuổi
+//	char* chuoi: địa chỉ bất đầu của chuổi
+// output: int số lượng ký tự
+int Diem_Ky_Tu(char* chuoi)
+{
+	int ketqua = 0;
+	while (chuoi[ketqua] != 0)
+	{
+		ketqua++;
+	}
+	return ketqua;
+}
+
 
 void main()
 {
-	//KHAI BÁO 10 đối tượng, mỗi đối tượng có kiểu dữ liệu là int
-	int arr[] = { 1,2,2,4,4,3,1,3,2,4,1,3 };
-
-	// tìm số lớn nhất trong mãng
-	int so_lon_nhat = 0;
-	int so_luong_doi_tuong = sizeof(arr)/sizeof(arr[0]);
-	for (int i = 0; i < so_luong_doi_tuong; i++)
-	{
-		if (so_lon_nhat < arr[i]) {
-			so_lon_nhat = arr[i];
-		}
-	}
-
-	// in ra casc vị trí của các số bằng với số lớn nhất
-	for (int i = 0; i < so_luong_doi_tuong; i++)
-	{
-		if(arr[i] == so_lon_nhat)
-			printf("%d ", i);
-	}
+	
 
 }
 
