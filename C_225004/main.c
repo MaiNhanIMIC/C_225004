178251e199eef93c558e2748059a705c1194f6e3
MaiNhanIMIC <mainhan.imic@gmail.com>
2025-04-08 20:22:50 +0700

    enum

--- C_225004/main.c
+++ C_225004/main.c
@@ -2,24 +2,71 @@
 #include <stdio.h>
 #include <string.h>
 
-#pragma pack(push)
-#pragma pack(1)
+typedef enum
+{
+	NAM, 
+	NU
+}gioiTinh_t;
+// muốn khai bao array, bao gồm nhiều đối tượng, mỗi đối tượng là 1 string (char*)
+char* convert_gioiTinh_string[] = { "Nam", "Nu" };
+
+typedef enum
+{
+	GIOI,
+	KHA,
+	TRUNG_BINH,
+	YEU
+} loai_t;
+char* convert_loai_string[] = { "Gioi", "Kha", "Trung Binh", "Yeu" };
 typedef struct
 {
-	int a;
-	char b;
-} test1_t;
-#pragma pack(pop)
+	char* ten;
+	int tuoi;
+	gioiTinh_t gioi_tinh;
+	float diem_toan;
+	float diem_van;
+	loai_t loai;
+} hoc_sinh_t;
 
-typedef struct
+void xep_loai(hoc_sinh_t* arr, int so_luong)
 {
-	int a;
-	char* b;
+	float TB = 0;
+	for (int i = 0; i < so_luong; i++)
+	{
+		TB = (arr[i].diem_toan + arr[i].diem_van) / 2;
+		if (TB >= 8)
+			arr[i].loai = GIOI;
+		else if(TB >= 6.5)
+			arr[i].loai = KHA;
+		else if (TB >= 5)
+			arr[i].loai = TRUNG_BINH;
+		else
+			arr[i].loai = YEU;
+	}
+}
 
-} test2_t;
+void In_DS(hoc_sinh_t* arr, int so_luong)
+{
+	for (int i = 0; i < so_luong; i++)
+	{
+		printf("Ten :%s\n", arr[i].ten);
+		printf("Tuoi:%d\n", arr[i].tuoi);
+		printf("Gioi tinh:%s\n", convert_gioiTinh_string[arr[i].gioi_tinh]);
+		printf("Diem toan:%f\n", arr[i].diem_toan);
+		printf("Diem van:%f\n", arr[i].diem_van);
+		printf("Loai:%s\n", convert_loai_string[arr[i].loai]);
+	}
+
+}
 
 void main()
 {
-	printf("size of test1_t: %d byte\n", sizeof(test1_t));
-	printf("size of test2_t: %d byte\n", sizeof(test2_t));
+	hoc_sinh_t arr[] = {
+		{"Nguyen Van A", 18, NAM, 8.5, 8.6},
+		{"Nguyen Thi B", 18, NU , 7.5, 7.0},
+		{"Nguyen Van C", 18, NAM, 6.5, 6.0}
+	};
+
+	xep_loai(arr , 3);
+	In_DS(arr, 3);
 }
