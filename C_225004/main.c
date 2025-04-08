aa11be9afaa1669e6aea3d6bbecbd949d159bd63
MaiNhanIMIC <mainhan.imic@gmail.com>
2025-04-08 19:33:24 +0700

    struct alignment

--- C_225004/main.c
+++ C_225004/main.c
@@ -1,72 +1,25 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+
+#pragma pack(push)
+#pragma pack(1)
+typedef struct
+{
+	int a;
+	char b;
+} test1_t;
+#pragma pack(pop)
 
 typedef struct
 {
-	int value;
-	void* previous_node;
-}node_t;
+	int a;
+	char* b;
 
-typedef struct
-{
-	node_t* last_node;
-	int len;
-}linked_list_t;
-/*
-3. Add: thêm một node vào cuối linked_list
-  - Input:
-   * linked_list_t* - địa chỉ đối tượng linked list. Cái mà chúng ta sẽ add node vào đó
-   * int - giá trị của node được add vào
-  - Output: void
-  - Gợi ý: sử dụng cấp phát động(malloc hoặc calloc) để tạo ra node, và gán previous_node lưu địa chỉ của node trước.
-
-*/
-void AddNodeToLinkList(linked_list_t* ll, int val)
-{
-	node_t* node = malloc(sizeof(node_t));
-	node->value = val;
-
-	if (ll->len > 0)
-	{
-		node->previous_node = ll->last_node;
-	}
-	else
-	{
-		node->previous_node = 0;
-	}
-	ll->last_node = node;
-	ll->len++;
-}
-
-/*
-9. GetValueIndex: lấy giá trị của node ở vị trí index
-  - Input:
-   * linked_list_t*: địa của của linked list cái mà chúng ta sẽ đi đọc giá trị của node ở vị trí index
-   * int index: vị trí của node chúng ta muốn đọc
-  - Ouput: int: giá trị của node mà chúng ta đọc được
-*/
-int GetValueIndex(linked_list_t* ll, int index)
-{
-	node_t* node_temp = ll->last_node;
-	for (int i = ll->len-1; i > index; i--)
-	{
-		node_temp = node_temp->previous_node;
-	}
-	return node_temp->value;
-}
-
+} test2_t;
 
 void main()
 {
-
-	linked_list_t ll = { 0 };
-	AddNodeToLinkList(&ll, 1);
-	AddNodeToLinkList(&ll, 2);
-	AddNodeToLinkList(&ll, 3);
-	AddNodeToLinkList(&ll, 4);
-	AddNodeToLinkList(&ll, 5);
-
-	int x = GetValueIndex(&ll, 3);
+	printf("size of test1_t: %d byte\n", sizeof(test1_t));
+	printf("size of test2_t: %d byte\n", sizeof(test2_t));
 }
