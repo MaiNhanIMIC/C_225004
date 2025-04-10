dd820c35a4de17ea15c998df93c97f3a5ac8693a
MaiNhanIMIC <mainhan.imic@gmail.com>
2025-04-10 21:09:52 +0700

    multi thread

--- C_225004/main.c
+++ C_225004/main.c
@@ -1,28 +1,45 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
+#include <Windows.h>
 
-typedef union
+int g_var = 0;
+HANDLE ReadPipe;
+HANDLE WritePipe;
+HANDLE mutex_lock;
+
+void print_log(char* log)
 {
-	struct {
-		char bit_0 : 1;
-		char bit_1 : 1;
-		char bit_2 : 1;
-		char bit_3 : 1;
-		char bit_4 : 1;
-		char bit_5 : 1;
-		char bit_6 : 1;
-		char bit_7 : 1;
-	};
-	char val;
-} byte_t;
+	int log_len = strlen(log);
+	for (int i = 0; i < log_len; i++)
+	{
+		printf("%c", log[i]);
+		Sleep(100);
+	}
+}
+
+DWORD WINAPI function_1(_In_ LPVOID lpParameter)
+{
+	while (1)
+	{
+		WaitForSingleObject(mutex_lock, INFINITE);
+		print_log("function_1 is running...\n");
+		ReleaseMutex(mutex_lock);
+		Sleep(500);
+	}
+}
+
 
 void main()
 {
-	byte_t A = { 0 };
-	A.bit_1 = 1;
-	A.bit_3 = 1;
-
-	printf("A: %d \n", A.val);
-
+	HANDLE thread_1 = CreateThread(NULL, 1024, function_1, NULL, 0,NULL);
+	mutex_lock = CreateMutex(NULL, 0, NULL);
+	
+	while (1)
+	{
+		WaitForSingleObject(mutex_lock, INFINITE);
+		print_log("main is running...\n");
+		ReleaseMutex(mutex_lock);
+		Sleep(1000);
+	}
 }
